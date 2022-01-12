9279a5e0d187ff2cc42a904f04642c341c74c8cb
noname_dev <noname_dev@naver.com>
2022-01-12 17:14:49 +0900

    Initial commit

---
+++ src/test/java/com/godofwarportfolio/godofwarportfolio/GodofwarportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godofwarportfolio.godofwarportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodofwarportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
