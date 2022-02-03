5b4c5b844179aed754fd68bbf9f18a5661176956
noname_dev <noname_dev@naver.com>
2022-02-03 21:47:28 +0900

    ProfileController 및 테스트 및 스프링 시큐리티 설정

--- src/test/java/com/godofwarportfolio/godofwarportfolio/web/ProfileControllerUnitTest.java
+++ src/test/java/com/godofwarportfolio/godofwarportfolio/web/ProfileControllerUnitTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mock.env.MockEnvironment;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -68,6 +70,14 @@ public class ProfileControllerUnitTest {
         assertThat(profile).isEqualTo(expectedProfile);
     }
 
+    @Test
+    public void profile은_인증없이_호출된다() throws Exception {
+        String expected = "default";
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
 
 
 }
