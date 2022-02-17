8f6a67d86f5b75c2a8e6e31b982e101d3abeb0e3
noname_dev <noname_dev@naver.com>
2022-02-17 21:29:14 +0900

    게임 소개 페이지

--- src/main/java/com/godofwarportfolio/godofwarportfolio/config/auth/SecurityConfig.java
+++ src/main/java/com/godofwarportfolio/godofwarportfolio/config/auth/SecurityConfig.java
@@ -20,7 +20,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/","/video/**","/bgm/**","/fullPage.js-master/**","/godjs/**"
-                        ,"/join/**","/css/**","/img/**", "/js/**","/h2-console/**","/profile","/character","/noticeBoard","/loginForm").permitAll()
+                        ,"/join/**","/css/**","/img/**", "/js/**","/h2-console/**","/profile","/character",
+                        "/noticeBoard","/loginForm","gameIntroduce").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and()
